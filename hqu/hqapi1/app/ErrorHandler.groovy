7b5f6ed776d7a4feed114a2bf954ae54ccda16bb
Ryan Morgan <rmorgan@hyperic.com>
2008-06-26 04:56:43 +0000

    Stub out User create API.

--- hqu/hqapi1/app/ErrorHandler.groovy
+++ hqu/hqapi1/app/ErrorHandler.groovy
@@ -2,7 +2,8 @@ class ErrorHandler {
     // List of all possible error conditions
     static codes =
     [LoginFailure: "The given username and password could not be validated",
-     ObjectNotFound: "The requested object could not be found"]
+     ObjectNotFound: "The requested object could not be found",
+     InvalidParameters: "The given parameters are incorrect"]
 
     static void printSuccessStatus(xmlOut) {
         xmlOut.Status("Success")
--- hqu/hqapi1/app/UserController.groovy
+++ hqu/hqapi1/app/UserController.groovy
@@ -7,7 +7,7 @@ import ErrorHandler
 class UserController extends BaseController
 {
     protected void init() {
-        setXMLMethods(['list', 'get'])
+        setXMLMethods(['list', 'get', 'create'])
     }
 
     private printUser(xmlOut, u) {
@@ -37,7 +37,7 @@ class UserController extends BaseController
     }
 
     def get(xmlOut, params) {
-        def name = params.getOne('name')
+        def name = params.getOne("name")
         def u = userHelper.findUser(name)
 
         if (u == null) {
@@ -51,6 +51,39 @@ class UserController extends BaseController
             }
         }
         
+        xmlOut
+    }
+
+    def create(xmlOut, params) {
+        // Required attributes
+        def name = params.getOne("Name")
+        def password = params.getOne("Password")
+        def first = params.getOne("FirstName")
+        def last = params.getOne("LastName")
+        def email = params.getOne("EmailAddress")
+
+        // Optional attributes
+        def htmlEmail = params.getOne("HtmlEmail", "false")
+        def active = params.getOne("Active", "true")
+        def dept = params.getOne("Department")
+        def phone = params.getOne("Phone")
+        def sms = params.getOne("SMSAddress")
+
+        // We require the user to authenticate via built in JDBC
+        def dsn = "CAM"
+
+        if (name == null || password == null || first == null ||
+            last == null || email == null) {
+            xmlOut.CreateUserResponse() {
+                ErrorHandler.printFailureStatus(xmlOut, "InvalidParameters")
+            }
+        } else {
+            // XXX: Do it
+            xmlOut.CreateUserResponse() {
+                ErrorHandler.printSuccessStatus(xmlOut)
+            }
+        }
+
         xmlOut
     }
 }
