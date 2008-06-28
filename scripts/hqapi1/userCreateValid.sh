86dfaab35b844d4869428c8b1696b23ac142727e
Ryan Morgan <rmorgan@hyperic.com>
2008-06-28 21:29:20 +0000

    Remove scripts, now moved into the hqu plugin.

--- scripts/hqapi1/userCreateValid.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-ID=`date +%s`
-NAME=apitest-${ID}
-
-curl -uhqadmin "http://localhost:7080/hqu/hqapi1/user/create.hqu?Name=${NAME}&FirstName=API&LastName=Test&Password=apitest&EmailAddress=apitest%40hyperic.com&Active=true"
--- scripts/hqapi1/userSync.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-DIR=`dirname $0`
-curl -uhqadmin http://localhost:7080/hqu/hqapi1/user/sync.hqu -Fpostdata=@"$DIR/userSync.xml"
