ba234df5644a9ae6f64bee8a0bbd2b64091b9964
Ryan Morgan <rmorgan@hyperic.com>
2008-06-28 17:57:39 +0000

    Add user sync tests.

--- src/com/hyperic/hq/hqapi1/test/UserSync_test.java
+++ src/com/hyperic/hq/hqapi1/test/UserSync_test.java
@@ -5,6 +5,7 @@ import org.hyperic.hq.hqapi1.jaxb.User;
 import org.hyperic.hq.hqapi1.jaxb.CreateUserResponse;
 import org.hyperic.hq.hqapi1.jaxb.ResponseStatus;
 import org.hyperic.hq.hqapi1.jaxb.SyncUserResponse;
+import org.hyperic.hq.hqapi1.jaxb.GetUserResponse;
 
 public class UserSync_test extends UserTestBase {
 
@@ -12,7 +13,7 @@ public class UserSync_test extends UserTestBase {
         super(name);
     }
 
-    public void testSyncFirstName() throws Exception {
+    public void testSync() throws Exception {
 
         HQApi api = getApi();
 
@@ -21,8 +22,58 @@ public class UserSync_test extends UserTestBase {
         CreateUserResponse createResponse = api.createUser(user, PASSWORD);
         assertEquals(ResponseStatus.SUCCESS, createResponse.getStatus());
 
-        user.setFirstName("Updated First Name");
+        String FIRST   = "Updated FirstName";
+        String LAST    = "Updated LastName";
+        String EMAIL   = "Updated EmailAddress";
+        String DEPT    = "Updated Department";
+        String SMS     = "Updated SMS";
+        String PHONE   = "Updated Phone";
+        boolean ACTIVE = !user.isActive();
+        boolean HTML   = !user.isHtmlEmail();
+
+        user.setFirstName(FIRST);
+        user.setLastName(LAST);
+        user.setEmailAddress(EMAIL);
+        user.setDepartment(DEPT);
+        user.setSMSAddress(SMS);
+        user.setPhoneNumber(PHONE);
+        user.setActive(ACTIVE);
+        user.setHtmlEmail(HTML);
         SyncUserResponse syncResponse = api.syncUser(user);
         assertEquals(ResponseStatus.SUCCESS, syncResponse.getStatus());
+
+        // Test the name has been updated
+        GetUserResponse getResponse = api.getUser(user.getName());
+        assertEquals(ResponseStatus.SUCCESS, getResponse.getStatus());
+        User u = getResponse.getUser();
+        assertEquals(FIRST,  u.getFirstName());
+        assertEquals(LAST,   u.getLastName());
+        assertEquals(EMAIL,  u.getEmailAddress());
+        assertEquals(DEPT,   u.getDepartment());
+        assertEquals(SMS,    u.getSMSAddress());
+        assertEquals(PHONE,  u.getPhoneNumber());
+        assertEquals(ACTIVE, u.isActive());
+        assertEquals(HTML,   u.isHtmlEmail());
+    }
+
+    public void testSyncNoPermission() throws Exception {
+
+        HQApi api = getApi();
+
+        User user = generateTestUser();
+
+        CreateUserResponse createResponse = api.createUser(user, PASSWORD);
+        assertEquals(ResponseStatus.SUCCESS, createResponse.getStatus());
+
+        // Reconnect as the new user
+
+        HQApi apiNewUser = getApi(user.getName(), PASSWORD);
+
+        User u = new User();
+        user.setName("hqadmin");
+        user.setFirstName("Updated FirstName");
+
+        SyncUserResponse syncResponse = apiNewUser.syncUser(u);
+        assertEquals(ResponseStatus.FAILURE, syncResponse.getStatus());
     }
 }
