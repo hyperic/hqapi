40049f72b9349337cb90404ee7ce94ac4eee9ebf
Ryan Morgan <rmorgan@hyperic.com>
2008-07-02 17:48:24 +0000

    Update UserApi doc to include link to Response.

--- src/com/hyperic/hq/hqapi1/UserApi.java
+++ src/com/hyperic/hq/hqapi1/UserApi.java
@@ -16,8 +16,9 @@ import java.util.HashMap;
  * The Hyperic HQ User API.
  *
  * This class provides access to the users within the HQ system.  Each of the
- * methods in this class return response objects that wrap the result of the
- * method with a {@link com.hyperic.hq.hqapi1.types.ResponseStatus} and a
+ * methods in this class return {@link com.hyperic.hq.hqapi1.types.Response}
+ * objects that wrap the result of the method with a
+ * {@link com.hyperic.hq.hqapi1.types.ResponseStatus} and a
  * {@link com.hyperic.hq.hqapi1.types.ServiceError} that indicates the error
  * if the response status is {@link com.hyperic.hq.hqapi1.types.ResponseStatus#FAILURE}.
  *
