8f3cc791733c0b756297c18c1623aaa08c330d9e
Jon Travis <jravis@hyperic.com>
2008-07-02 23:31:43 +0000

    Redo the way XML is streamed, now using the renderXml() method

--- hqu/hqapi1/app/ApiController.groovy
+++ hqu/hqapi1/app/ApiController.groovy
@@ -1,30 +1,42 @@
 import org.hyperic.hq.hqu.rendit.BaseController
 
+import groovy.xml.StreamingMarkupBuilder
+
 class ApiController extends BaseController {
     
     // List of all possible error conditions
-    final CODES =
-    [LoginFailure: "The given username and password could not be validated",
+    final CODES = [
+     LoginFailure: "The given username and password could not be validated",
      ObjectNotFound: "The requested object could not be found",
      ObjectExists: "The given object already exists",
      InvalidParameters: "The given parameters are incorrect",
      UnexpectedError: "An unexpected error occured",
-     PermissionDenied: "Permission denied"]
+     PermissionDenied: "Permission denied"
+    ]
 
-    void printSuccessStatus(xmlOut) {
-        xmlOut.Status("Success")
+    protected Closure getSuccessXML() {
+        { doc -> 
+            Status("Success")
+        }
     }
 
-    void printFailureStatus(xmlOut, code) {
+    /**
+     * Add a failure code to a chunk of XML.
+     * 
+     * @param code  Must be a code from CODES
+     */
+    protected Closure getFailureXML(String code) {
         def reason = CODES.get(code)
         if (reason == null) {
-            throw new IllegalArgumentException("Invalid ErrorCode: " + code)
+            throw new IllegalArgumentException("Invalid ErrorCode: [${code}]")
         }
 
-        xmlOut.Status("Failure")
-        xmlOut.Error() {
-            ErrorCode(code)
-            ReasonText(reason)
+        { doc ->
+            Status("Failure")
+            Error() {
+                ErrorCode(code)
+                ReasonText(reason)
+            }
         }
     }
 
--- hqu/hqapi1/app/UserController.groovy
+++ hqu/hqapi1/app/UserController.groovy
@@ -2,153 +2,147 @@ import org.hyperic.hq.hqu.rendit.BaseController
 
 import org.hyperic.hq.authz.shared.PermissionException;
 
-class UserController extends ApiController
-{
-    protected void init() {
-        setXMLMethods(['get', 'create', 'delete', 'update'])
-    }
-
-    private printUser(xmlOut, u) {
-        xmlOut.User(id:u.id,
-                    name:u.name,
-                    firstName: u.firstName,
-                    lastName: u.lastName,
-                    department: (u.department ? u.department : ''),
-                    email: u.emailAddress,
-                    smsAddress: (u.SMSAddress ? u.SMSAddress : ''),
-                    phoneNumber: (u.phoneNumber ? u.phoneNumber : ''),
-                    active: u.active,
-                    htmlEmail: u.htmlEmail)
+class UserController extends ApiController {
+    private Closure getUserXML(u) {
+        { doc -> 
+            User(id          : u.id,
+                 name        : u.name,
+                 firstName   : u.firstName,
+                 lastName    : u.lastName,
+                 department  : (u.department ? u.department : ''),
+                 email       : u.emailAddress,
+                 smsAddress  : (u.SMSAddress ? u.SMSAddress : ''),
+                 phoneNumber : (u.phoneNumber ? u.phoneNumber : ''),
+                 active      : u.active,
+                 htmlEmail   : u.htmlEmail)
+        }
     }
 
     def list(params) {
-        def users = userHelper.allUsers
-
-        renderXml() { xmlOut ->
-            GetUsersResponse() {    
-                printSuccessStatus(xmlOut)
-                for (u in users.sort {a, b -> a.name <=> b.name}) {
-                    printUser(xmlOut, u)
+        renderXml() {
+            out << GetUsersResponse() {
+                out << getSuccessXML()
+                for (u in userHelper.allUsers.sort {a, b -> a.name <=> b.name}) {
+                    out << getUserXML(u)
                 }
             }
         }
     }
 
-    def get(xmlOut, params) {
+    def get(params) {
         def name = params.getOne("name")
-        def u = userHelper.findUser(name)
+        def u    = userHelper.findUser(name)
 
-        if (u == null) {
-            xmlOut.GetUserResponse() {
-                printFailureStatus(xmlOut, "ObjectNotFound")
-            }
-        } else {
-            xmlOut.GetUserResponse() {
-                printSuccessStatus(xmlOut)
-                printUser(xmlOut, u)
+        renderXml() {
+            GetUserResponse() {
+                if (!u) {
+                    out << getFailureXML("ObjectNotFound")
+                } else {
+                    out << getSuccessXML()
+                    out << getUserXML(u)
+                }
             }
         }
-        
-        xmlOut
     }
 
-    def create(xmlOut, params) {
+    def create(params) {
         // Required attributes
-        def name = params.getOne("name")
+        def name     = params.getOne("name")
         def password = params.getOne("password")
-        def first = params.getOne("firstName")
-        def last = params.getOne("lastName")
-        def email = params.getOne("emailAddress")
+        def first    = params.getOne("firstName")
+        def last     = params.getOne("lastName")
+        def email    = params.getOne("emailAddress")
 
         // Optional attributes
         def htmlEmail = params.getOne("htmlEmail", "false").toBoolean()
-        def active = params.getOne("active", "false").toBoolean()
-        def dept = params.getOne("department")
-        def phone = params.getOne("phoneNumber")
-        def sms = params.getOne("SMSAddress")
+        def active    = params.getOne("active", "false").toBoolean()
+        def dept      = params.getOne("department")
+        def phone     = params.getOne("phoneNumber")
+        def sms       = params.getOne("SMSAddress")
 
         // We require the user to authenticate via built in JDBC
         def dsn = "CAM"
 
-        if (name == null || password == null || first == null ||
-            last == null || email == null) {
-            xmlOut.CreateUserResponse() {
-                printFailureStatus(xmlOut, "InvalidParameters")
-            }
+        def failureXml
+        
+        if (!name || !password || !first || !last || !email) {
+            failureXml = getFailureXML("InvalidParameters")
         } else {
             try {
                 def existing = userHelper.findUser(name)
                 if (existing) {
-                    xmlOut.CreateUserResponse() {
-                        printFailureStatus(xmlOut, "ObjectExists")
-                    }
+                    failureXml = getFailureXML("ObjectExists")
                 } else {
-                    userHelper.createUser(name, password, active, dsn, dept,
-                                          email, first, last, phone, sms,
-                                          htmlEmail)
-                    xmlOut.CreateUserResponse() {
-                        printSuccessStatus(xmlOut)
-                    }
+                    userHelper.createUser(name, password, active,
+                                          dsn, dept, email, first,
+                                          last, phone, sms, htmlEmail)
                 }
             } catch (Exception e) {
                 log.error("UnexpectedError: " + e.getMessage(), e);
-                xmlOut.CreateUserResponse() {
-                    printFailureStatus(xmlOut, "UnexpectedError")
+                failureXml = getFailureXML("UnexpectedError")
+            }
+        }
+        
+        renderXml() {
+            CreateUserResponse() {
+                if (failureXml) {
+                    out << failureXml
+                } else {
+                    out << getSuccessXML()
                 }
             }
         }
-
-        xmlOut
     }
 
-    def delete(xmlOut, params) {
+    def delete(params) {
         def name = params.getOne('name')
 
         def existing = userHelper.findUser(name)
+        def failureXml
+        
         if (!existing) {
-            xmlOut.DeleteUserResponse() {
-                printFailureStatus(xmlOut, "ObjectNotFound")
-            }
+            failureXml = getFailureXML("ObjectNotFound")
         } else {
             try {
                 userHelper.removeUser(existing.id)
-                xmlOut.DeleteUserResponse() {
-                    printSuccessStatus(xmlOut)
-                }
             } catch (Exception e) {
                 log.error("UnexpectedError: " + e.getMessage(), e)
-                xmlOut.DeleteUserResponse() {
-                    printFailureStatus(xmlOut, "UnexpectedError")
+                failureXml = getFailureXML("UnexpectedError")
+            }
+        }
+        
+        renderXml() {
+            DeleteUserResponse() {
+                if (failureXml) {
+                    out << failureXml
+                } else {
+                    out << getSuccessXML()
                 }
             }
         }
-
-        xmlOut
     }
 
-    def update(xmlOut, params) {
-
+    def update(params) {
+        def failureXml
         try {
             def updateRequest = new XmlParser().parseText(getUpload('postdata'))
             def xmlUser = updateRequest['User']
             
-            if (xmlUser == null || xmlUser.size() != 1) {
-                xmlOut.UpdateUserResponse() {
-                    printFailureStatus(xmlOut, 'InvalidParameters')
+            if (!xmlUser || xmlUser.size() != 1) {
+                renderXml() {
+                    UpdateUserResponse() {
+                        out << getFailureXML('InvalidParameters')
+                    }
                 }
-                return xmlOut
+                return
             }
             
             def xmlIn = xmlUser[0]
 
             def name = xmlIn.'@name'
-            log.info "xmlIn = ${xmlIn}"
-            log.info "xmlIn.active = ${xmlIn.'@active'}"
             def existing = userHelper.findUser(name)
             if (!existing) {
-                xmlOut.UpdateUserResponse() {
-                    printFailureStatus(xmlOut, "ObjectNotFound")
-                }
+                failureXml = getFailureXML("ObjectNotFound")
             } else {
                 userHelper.updateUser(existing,
                                       xmlIn.'@active'?.toBoolean(),
@@ -160,21 +154,24 @@ class UserController extends ApiController
                                       xmlIn.'@phoneNumber',
                                       xmlIn.'@SMSAddress',
                                       xmlIn.'@htmlEmail'?.toBoolean())
-                xmlOut.UpdateUserResponse() {
-                    printSuccessStatus(xmlOut)
-                }
             }
         } catch (PermissionException e) {
-            xmlOut.UpdateUserResponse() {
-                printFailureStatus(xmlOut, "PermissionDenied")
-            }
+            log.error("Permission denied [${user.name}]", e)
+            failureXml = getFailureXML("PermissionDenied")
         } catch (Exception e) {
             log.error("UnexpectedError: " + e.getMessage(), e)
-            xmlOut.UpdateUserResponse() {
-                printFailureStatus(xmlOut, "UnexpectedError")
-            }
+            failureXml = getFailureXML("UnexpectedError")
         }
 
+        renderXml() {
+            UpdateUserResponse() {
+                if (failureXml) {
+                    out << failureXml
+                } else {
+                    out << getSuccessXML()
+                }
+            }
+        }
         xmlOut
     }
 }
