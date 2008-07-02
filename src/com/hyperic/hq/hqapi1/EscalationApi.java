0a920ad27d3c6fb08c95613e8faa0be8284a5d17
Charles Lee <clee@hyperic.com>
2008-07-02 21:23:34 +0000

    Add standard APIs

--- src/com/hyperic/hq/hqapi1/EscalationApi.java
+++ src/com/hyperic/hq/hqapi1/EscalationApi.java
@@ -1,13 +1,16 @@
 package com.hyperic.hq.hqapi1;
 
 import java.io.IOException;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.hyperic.hq.hqapi1.types.CreateEscalationResponse;
 import com.hyperic.hq.hqapi1.types.DeleteEscalationResponse;
 import com.hyperic.hq.hqapi1.types.Escalation;
 import com.hyperic.hq.hqapi1.types.GetEscalationResponse;
 import com.hyperic.hq.hqapi1.types.SyncEscalationResponse;
+import com.hyperic.hq.hqapi1.types.UpdateEscalationResponse;
 
 /**
  * The Hyperic HQ Escalation API.
@@ -28,6 +31,34 @@ public class EscalationApi {
     }
     
     /**
+     * Find a {@link com.hyperic.hq.hqapi1.types.Escalation} by ID.
+     *
+     * @param id The escalation ID to search for.
+     * @return On {@link com.hyperic.hq.hqapi1.types.ResponseStatus#SUCCESS},
+     * the Escalation by the given name is returned via
+     * {@link com.hyperic.hq.hqapi1.types.GetEscalationResponse#getEscalation()}.
+     *
+     * On {@link com.hyperic.hq.hqapi1.types.ResponseStatus#FAILURE} the
+     * following error codes can be returned:
+     *
+     * <p>
+     * <ul>
+     *   <li>LoginFailure - The given username and password could not be validated.
+     *   <li>ObjectNotFound - The given escalation was not found.
+     *   <li>UnexpectedError - Any other internal server error.
+     * </ul>
+     * </p>
+     * @throws IOException If a network error occurs while making the request.
+     */
+    public GetEscalationResponse getEscalation(Integer id)
+        throws IOException {
+        Map<String, Integer> params = new HashMap<String, Integer>();
+        params.put("id", id);
+        return _connection.doGet("/hqu/hqapi1/escalation/get.hqu",
+                                 params, GetEscalationResponse.class);
+    }
+
+    /**
      * Find a {@link com.hyperic.hq.hqapi1.types.Escalation} by name.
      *
      * @param name The escalation name to search for.
@@ -46,7 +77,7 @@ public class EscalationApi {
      * </p>
      * @throws IOException If a network error occurs while making the request.
      */
-    public GetEscalationResponse getEscalationByName(String name)
+    public GetEscalationResponse getEscalation(String name)
         throws IOException {
         Map<String, String> params = new HashMap<String, String>();
         params.put("name", name);
@@ -55,9 +86,59 @@ public class EscalationApi {
     }
     
     /**
-     * Sync an {@link Escalation} by creating or updating
+     * Create an {@link Escalation}
      *
-     * @param esc The escalation name to sync.
+     * @param esc The escalation to create.
+     * @return {@link com.hyperic.hq.hqapi1.types.ResponseStatus#SUCCESS} if the
+     * escalation was created successfully.
+     *
+     * On {@link com.hyperic.hq.hqapi1.types.ResponseStatus#FAILURE} the
+     * following error codes can be returned:
+     *
+     * <p>
+     * <ul>
+     *   <li>LoginFailure - The given username and password could not be validated.
+     *   <li>UnexpectedError - Any other internal server error.
+     * </ul>
+     * </p>
+     * @throws IOException If a network error occurs while making the request.
+     */
+    public CreateEscalationResponse createEscalation(Escalation esc)
+        throws IOException {
+        Map<String, String> params = new HashMap<String, String>();
+        return _connection.doGet("/hqu/hqapi1/escalation/update.hqu",
+                                 params, CreateEscalationResponse.class);
+    }
+
+    /**
+     * Update the properties of an {@link Escalation}
+     *
+     * @param esc The escalation to update.
+     * @return {@link com.hyperic.hq.hqapi1.types.ResponseStatus#SUCCESS} if the
+     * escalation was updated successfully.
+     *
+     * On {@link com.hyperic.hq.hqapi1.types.ResponseStatus#FAILURE} the
+     * following error codes can be returned:
+     *
+     * <p>
+     * <ul>
+     *   <li>LoginFailure - The given username and password could not be validated.
+     *   <li>UnexpectedError - Any other internal server error.
+     * </ul>
+     * </p>
+     * @throws IOException If a network error occurs while making the request.
+     */
+    public UpdateEscalationResponse updateEscalation(Escalation esc)
+        throws IOException {
+        Map<String, String> params = new HashMap<String, String>();
+        return _connection.doGet("/hqu/hqapi1/escalation/update.hqu",
+                                 params, UpdateEscalationResponse.class);
+    }
+
+    /**
+     * Sync a collection of {@link Escalation}s by creating or updating
+     *
+     * @param escs The collections of escalations to sync.
      * @return {@link com.hyperic.hq.hqapi1.types.ResponseStatus#SUCCESS} if the
      * escalation was synchronized successfully.
      *
@@ -72,7 +153,7 @@ public class EscalationApi {
      * </p>
      * @throws IOException If a network error occurs while making the request.
      */
-    public SyncEscalationResponse syncEscalation(Escalation esc)
+    public SyncEscalationResponse syncEscalation(Collection<Escalation> escs)
         throws IOException {
         Map<String, String> params = new HashMap<String, String>();
         return _connection.doGet("/hqu/hqapi1/escalation/sync.hqu",
@@ -82,7 +163,7 @@ public class EscalationApi {
     /**
      * Delete an {@link Escalation}
      *
-     * @param name The escalation name to delete.
+     * @param esc The escalation to delete.
      * @return {@link com.hyperic.hq.hqapi1.types.ResponseStatus#SUCCESS} if the
      * escalation was deleted successfully.
      *
@@ -98,10 +179,9 @@ public class EscalationApi {
      * </p>
      * @throws IOException If a network error occurs while making the request.
      */
-    public DeleteEscalationResponse deleteEscalation(String name)
+    public DeleteEscalationResponse deleteEscalation(Escalation esc)
         throws IOException {
         Map<String, String> params = new HashMap<String, String>();
-        params.put("name", name);
         return _connection.doGet("/hqu/hqapi1/escalation/delete.hqu",
                                  params, DeleteEscalationResponse.class);
     }
