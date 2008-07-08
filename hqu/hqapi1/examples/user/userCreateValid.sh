319a94fa9c557286dbd7094588c5da3bd55edc03
Ryan Morgan <rmorgan@hyperic.com>
2008-07-08 15:42:23 +0000

    Fix script examples.

--- hqu/hqapi1/examples/user/userCreateValid.sh
+++ hqu/hqapi1/examples/user/userCreateValid.sh
@@ -3,4 +3,4 @@
 ID=`date +%s`
 NAME=apitest-${ID}
 
-curl -uhqadmin "http://localhost:7080/hqu/hqapi1/user/create.hqu?Name=${NAME}&FirstName=API&LastName=Test&Password=apitest&EmailAddress=apitest%40hyperic.com&Active=true"
+curl -uhqadmin "http://localhost:7080/hqu/hqapi1/user/create.hqu?name=${NAME}&firstName=API&lastName=Test&password=apitest&emailAddress=apitest%40hyperic.com&Active=true"
