cf417d8baeb438f42e532e5ca5dd0bfe97b63dde
Charles Lee <clee@hyperic.com>
2008-07-09 22:16:54 +0000

    Add Escalation controller to HQU

---
+++ hqu/hqapi1/app/EscalationController.groovy
@@ -0,0 +1,58 @@
+import org.hyperic.hq.hqu.rendit.BaseController
+
+import org.hyperic.hq.authz.shared.PermissionException;
+import org.hyperic.hq.hqapi1.ErrorCode;
+
+class EscalationController extends ApiController {
+    private Closure getEscalationXML(e) {
+        { doc -> 
+            Escalation(id :           e.id,
+                       name :         e.name,
+                       description :  e.description,
+                       pauseAllowed : e.pauseAllowed,
+                       maxPauseTime : e.maxPauseTime,
+                       notifyAll :    e.notifyAll,
+                       repeat :       e.repeat)
+        }
+    }
+
+    def get(params) {
+        def id = params.getOne("id")?.toInteger()
+        def name = params.getOne("name")
+        
+        def esc = escalationHelper.getEscalation(id, name)
+
+        renderXml() {
+            out << GetEscalationResponse() {
+                if (!esc) {
+                    out << getFailureXML(ErrorCode.OBJECT_NOT_FOUND)
+                }
+                else {
+                    out << getSuccessXML()
+                    out << getEscalationXML(esc)
+                }
+            }
+        }
+    }
+
+    def list(params) {
+        renderXml() {
+            out << GetEscalationrsResponse() {
+                out << getSuccessXML()
+                for (e in escalationHelper.allEscalations.sort {a, b -> a.name <=> b.name}) {
+                    out << getEscalationXML(e)
+                }
+            }
+        }
+    }
+
+    def delete(params) {
+        def id = params.getOne("id").toInteger()
+        def esc = escalationHelper.deleteEscalation(id)
+        renderXml() {
+            out << DeleteEscalationResponse() {
+                out << getSuccessXML()
+            }
+        }
+    }
+}
