2a7287f4eb424f4c988b744553a7b97ce2673a86
Ryan Morgan <rmorgan@hyperic.com>
2008-07-25 22:26:57 +0000

    Test getting a valid agent entity.

--- src/org/hyperic/hq/hqapi1/test/AgentGet_test.java
+++ src/org/hyperic/hq/hqapi1/test/AgentGet_test.java
@@ -2,6 +2,7 @@ package org.hyperic.hq.hqapi1.test;
 
 import org.hyperic.hq.hqapi1.AgentApi;
 import org.hyperic.hq.hqapi1.types.GetAgentResponse;
+import org.hyperic.hq.hqapi1.types.Agent;
 
 public class AgentGet_test extends HQApiTestBase {
 
@@ -15,5 +16,27 @@ public class AgentGet_test extends HQApiTestBase {
 
         GetAgentResponse response = api.getAgent("invalid.hyperic.com", 2144);
         hqAssertFailureObjectNotFound(response);
+    }
+
+    public void testGetAgentValid() throws Exception {
+
+        Agent agent = getLocalAgent();
+
+        if (agent == null) {
+            getLog().warn("No agent running, skipping test.");
+            return;
+        }
+
+        AgentApi api = getApi().getAgentApi();
+
+        GetAgentResponse response = api.getAgent(agent.getAddress(),
+                                                 agent.getPort());
+        hqAssertSuccess(response);
+
+        Agent a = response.getAgent();
+        assertTrue(a.getAddress().length() > 0);
+        assertTrue(a.getPort() > 0);
+        assertTrue(a.getVersion().length() > 0);
+        assertTrue(a.getId() > 0);
     }
 }
