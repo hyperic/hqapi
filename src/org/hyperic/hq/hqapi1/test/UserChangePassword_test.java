1798520afa0cb54d7fb626e5099ebc73584af61c
Ryan Morgan <rmorgan@hyperic.com>
2008-07-25 23:48:25 +0000

    Simplify using base helpers.

--- src/org/hyperic/hq/hqapi1/test/UserChangePassword_test.java
+++ src/org/hyperic/hq/hqapi1/test/UserChangePassword_test.java
@@ -18,19 +18,15 @@ public class UserChangePassword_test extends UserTestBase {
         UserApi api = getUserApi();
 
         // Create a new user.
-        User u = generateTestUser();
-        CreateUserResponse createResponse = api.createUser(u, PASSWORD);
-        hqAssertSuccess(createResponse);
+        User u = createTestUsers(1).get(0);
 
         final String NEWPASS = "NEWPASSWORD";
         // Change that users password.
-        User createdUser = createResponse.getUser();
-        ChangePasswordResponse response = api.changePassword(createdUser,
-                                                             NEWPASS);
+        ChangePasswordResponse response = api.changePassword(u, NEWPASS);
         hqAssertSuccess(response);
 
         // Log in as the new user and list the users.
-        UserApi api2 = getUserApi(createdUser.getName(), NEWPASS);
+        UserApi api2 = getUserApi(u.getName(), NEWPASS);
         GetUsersResponse getResponse = api2.getUsers();
         hqAssertSuccess(getResponse);
     }
@@ -39,30 +35,18 @@ public class UserChangePassword_test extends UserTestBase {
 
         UserApi api = getUserApi();
 
-        // Create a new user.
-        User u = generateTestUser();
-        CreateUserResponse createResponse = api.createUser(u, PASSWORD);
-        hqAssertSuccess(createResponse);
-
-        // Change that users password.
-        User createdUser = createResponse.getUser();
-        ChangePasswordResponse response = api.changePassword(createdUser,
-                                                             "");
+        // Test changing a password to an empty string.
+        User u = createTestUsers(1).get(0);
+        ChangePasswordResponse response = api.changePassword(u, "");
         hqAssertFailureInvalidParameters(response);        
     }
 
     public void testChangePasswordNull() throws Exception {
         UserApi api = getUserApi();
 
-        // Create a new user.
-        User u = generateTestUser();
-        CreateUserResponse createResponse = api.createUser(u, PASSWORD);
-        hqAssertSuccess(createResponse);
-
-        // Change that users password.
-        User createdUser = createResponse.getUser();
-        ChangePasswordResponse response = api.changePassword(createdUser,
-                                                             null);
+        // Test changing a password to a null string.
+        User u = createTestUsers(1).get(0);
+        ChangePasswordResponse response = api.changePassword(u, null);
         hqAssertFailureInvalidParameters(response);
     }
 }
