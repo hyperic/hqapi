cbe5386773c2e7f4e4552af00cfeeeb0d5e4eff8
Ilaya Perumal <Ilaya.Perumal@hyperic.com>
2008-08-18 15:59:02 +0000

    Escalation retrieval examples

---
+++ hqu/hqapi1/examples/escalation/escalationGetById.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+curl -uhqadmin http://localhost:7080/hqu/hqapi1/escalation/get.hqu?id=100
---
+++ hqu/hqapi1/examples/escalation/escalationGetByName.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+curl -uhqadmin http://localhost:7080/hqu/hqapi1/escalation/get.hqu?name=Default%20Escalation
