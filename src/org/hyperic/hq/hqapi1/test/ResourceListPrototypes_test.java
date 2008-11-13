154f428463d4d357ab5a386e7710f62da49b23f3
Ryan Morgan <rmorgan@hyperic.com>
2008-11-13 17:36:54 +0000

    Additional ResourcePrototype checks.

--- src/org/hyperic/hq/hqapi1/test/ResourceListPrototypes_test.java
+++ src/org/hyperic/hq/hqapi1/test/ResourceListPrototypes_test.java
@@ -2,6 +2,7 @@ package org.hyperic.hq.hqapi1.test;
 
 import org.hyperic.hq.hqapi1.ResourceApi;
 import org.hyperic.hq.hqapi1.types.ListResourcePrototypesResponse;
+import org.hyperic.hq.hqapi1.types.ResourcePrototype;
 
 public class ResourceListPrototypes_test extends HQApiTestBase {
 
@@ -16,5 +17,10 @@ public class ResourceListPrototypes_test extends HQApiTestBase {
         
         hqAssertSuccess(resp);
         assertTrue(resp.getResourcePrototype().size() != 0);
+
+        for (ResourcePrototype pt : resp.getResourcePrototype()) {
+            assertTrue(pt.getId() > 0);
+            assertTrue(pt.getName() != null && pt.getName().length() > 0);
+        }
     }
 }
