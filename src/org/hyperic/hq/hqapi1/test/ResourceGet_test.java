3dd0e5aad6635f3c1f5a75d885b3ed9b8857625f
Ryan Morgan <rmorgan@hyperic.com>
2008-11-13 21:14:56 +0000

    Add testGetResource()

--- src/org/hyperic/hq/hqapi1/test/ResourceGet_test.java
+++ src/org/hyperic/hq/hqapi1/test/ResourceGet_test.java
@@ -2,6 +2,9 @@ package org.hyperic.hq.hqapi1.test;
 
 import org.hyperic.hq.hqapi1.ResourceApi;
 import org.hyperic.hq.hqapi1.types.GetResourceResponse;
+import org.hyperic.hq.hqapi1.types.Agent;
+import org.hyperic.hq.hqapi1.types.FindResourcesResponse;
+import org.hyperic.hq.hqapi1.types.Resource;
 
 public class ResourceGet_test extends HQApiTestBase {
 
@@ -15,5 +18,38 @@ public class ResourceGet_test extends HQApiTestBase {
 
         GetResourceResponse resp = api.getResource(Integer.MAX_VALUE);
         hqAssertFailureObjectNotFound(resp);
+    }
+
+    public void testGetResource() throws Exception {
+
+        Agent a = getLocalAgent();
+
+        if (a == null) {
+            getLog().warn("No local agent found, skipping test.");
+            return;
+        }
+
+        ResourceApi api = getApi().getResourceApi();
+
+        FindResourcesResponse findResponse = api.findResources(a);
+        hqAssertSuccess(findResponse);
+
+        // This test assumes if you have a local agent that is pingable that
+        // there will be at least one platform servicing it.
+        assertTrue("Found 0 platform resources for agent " + a.getId(),
+                   findResponse.getResource().size() > 0);
+        for (Resource r : findResponse.getResource()) {
+            // Now that we have valid resource ids, query each
+
+            Integer rid = r.getId();
+
+            GetResourceResponse getResponse = api.getResource(rid);
+            hqAssertSuccess(getResponse);
+            Resource resource = getResponse.getResource();
+            assertNotNull(resource);
+            assertTrue("Invalid resource id " + r.getId(),
+                       r.getId() > 0);
+            assertNotNull(resource.getName());
+        }
     }
 }
