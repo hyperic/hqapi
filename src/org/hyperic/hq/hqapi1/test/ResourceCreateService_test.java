449e29f28a949f852a1cd777ab4937755ccdfa97
Ryan Morgan <rmorgan@hyperic.com>
2008-11-15 00:43:00 +0000

    Checkpoint service create.

--- src/org/hyperic/hq/hqapi1/test/ResourceCreateService_test.java
+++ src/org/hyperic/hq/hqapi1/test/ResourceCreateService_test.java
@@ -4,8 +4,12 @@ import org.hyperic.hq.hqapi1.ResourceApi;
 import org.hyperic.hq.hqapi1.types.Agent;
 import org.hyperic.hq.hqapi1.types.Resource;
 import org.hyperic.hq.hqapi1.types.ResourcePrototype;
+import org.hyperic.hq.hqapi1.types.GetResourcePrototypeResponse;
+import org.hyperic.hq.hqapi1.types.FindResourcesResponse;
+import org.hyperic.hq.hqapi1.types.CreateResourceResponse;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class ResourceCreateService_test extends ResourceTestBase {
 
@@ -21,14 +25,40 @@ public class ResourceCreateService_test extends ResourceTestBase {
             return;
         }
 
-        Resource parent = new Resource();
-        parent.setName("My Server");
-        ResourcePrototype pt = new ResourcePrototype();
-        pt.setName("HTTP");
-
         ResourceApi api = getApi().getResourceApi();
-        api.createService(pt, parent, "My Service",
-                          "A test service created by the test suite",
-                          new HashMap<String,String>());
+
+        GetResourcePrototypeResponse protoResponse = api.getResourcePrototype("HTTP");
+        hqAssertSuccess(protoResponse);
+        ResourcePrototype pt = protoResponse.getResourcePrototype();
+
+        FindResourcesResponse resourcesResponse = api.findResources(a);
+        hqAssertSuccess(resourcesResponse);
+        String PARENT_TYPE = "Net Services";
+        Resource parent = null;
+        for (Resource r : resourcesResponse.getResource()) {
+            if (r.getResourcePrototype().getName().equals("Net Services")) {
+                parent = r;
+                break;
+            }
+        }
+
+        if (parent == null) {
+            getLog().error("Unable to find server of type " + PARENT_TYPE +
+                           " skipping test.");
+            return;
+        }
+
+        Map<String,String> params = new HashMap<String,String>();
+        params.put("hostname", "www.hyperic.com");
+        params.put("port", "80");
+        params.put("sotimeout", "10");
+        params.put("path", "/");
+        params.put("method", "GET");
+
+        CreateResourceResponse resp =
+                api.createService(pt, parent, "My HTTP Check",
+                                  "A test service created by the test suite",
+                                  params);
+        hqAssertFailureNotImplemented(resp);
     }
 }
