1b99aa61cd14e456b9c1a0234713a386f90f0c67
Ryan Morgan <rmorgan@hyperic.com>
2008-12-15 22:43:30 +0000

    Add getRequiredOptionValue(Option)

--- src/org/hyperic/hq/hqapi1/tools/Parser.java
+++ src/org/hyperic/hq/hqapi1/tools/Parser.java
@@ -4,11 +4,13 @@ import jargs.gnu.CmdLineParser;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
 
 public class Parser extends CmdLineParser {
 
-    private List<String> _requiredHelp = new ArrayList<String>();
-    private List<String> _optionalHelp = new ArrayList<String>();
+    private Map<Option,String> _requiredHelp = new HashMap<Option,String>();
+    private Map<Option,String> _optionalHelp = new HashMap<Option,String>();
 
     public Option addOption(Option option, boolean required, String helpString)
     {
@@ -22,25 +24,36 @@ public class Parser extends CmdLineParser {
             help = "--" + option.longForm() + ": " + helpString;
         }
         if (required) {
-            _requiredHelp.add(help);
+            _requiredHelp.put(option, help);
         } else {
-            _optionalHelp.add(help);
+            _optionalHelp.put(option, help);
         }
 
         return option;
+    }
+
+    public Object getRequiredOptionValue(Option opt) {
+        Object o = getOptionValue(opt);
+
+        if (o == null && _requiredHelp.containsKey(opt)) {
+            System.err.println("Required argument " + opt.longForm() + " not given");
+            System.exit(-1);
+        }
+
+        return o;
     }
 
     public void printUsage() {
         System.err.println("Usage: prog [arguments]");
         System.err.println("Required arguments:");
 
-        for (String help : _requiredHelp) {
-            System.err.println(help);
+        for (Option opt : _requiredHelp.keySet()) {
+            System.err.println(_requiredHelp.get(opt));
         }
 
         System.err.println("Optional arguments:");
-        for (String help : _optionalHelp) {
-            System.err.println(help);
+        for (Option opt : _optionalHelp.keySet()) {
+            System.err.println(_optionalHelp.get(opt));
         }
     }
 }
