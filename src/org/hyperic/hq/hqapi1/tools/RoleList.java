f5a46d7e2994b3f4cf2584c989b78ed124fa37eb
Ryan Morgan <rmorgan@hyperic.com>
2008-12-17 22:08:43 +0000

    Add role list/sync.

---
+++ src/org/hyperic/hq/hqapi1/tools/RoleList.java
@@ -0,0 +1,64 @@
+package org.hyperic.hq.hqapi1.tools;
+
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+import org.hyperic.hq.hqapi1.HQApi;
+import org.hyperic.hq.hqapi1.XmlUtil;
+import org.hyperic.hq.hqapi1.RoleApi;
+import org.hyperic.hq.hqapi1.types.ResponseStatus;
+import org.hyperic.hq.hqapi1.types.RolesResponse;
+import org.hyperic.hq.hqapi1.types.RoleResponse;
+
+public class RoleList extends ToolsBase {
+
+    private static String OPT_ID = "id";
+    private static String OPT_NAME = "name";
+
+    private static void listRoles(String[] args) throws Exception {
+
+        OptionParser p = getOptionParser();
+
+        p.accepts(OPT_ID, "If specified, return the role with the given id.").
+                withRequiredArg().ofType(Integer.class);
+        p.accepts(OPT_NAME, "If specified, return the role with the given name").
+                withRequiredArg().ofType(String.class);
+
+        OptionSet options = getOptions(p, args);
+
+        HQApi api = getApi(options);
+        RoleApi roleApi = api.getRoleApi();
+
+        RolesResponse roles;
+
+        if (options.has(OPT_ID)) {
+            Integer id = (Integer) options.valueOf(OPT_ID);
+            RoleResponse role = roleApi.getRole(id);
+            checkSuccess(role);
+            roles = new RolesResponse();
+            roles.setStatus(ResponseStatus.SUCCESS);
+            roles.getRole().add(role.getRole());
+        } else if (options.has(OPT_NAME)) {
+            String name = (String) options.valueOf(OPT_NAME);
+            RoleResponse role = roleApi.getRole(name);
+            checkSuccess(role);
+            roles = new RolesResponse();
+            roles.setStatus(ResponseStatus.SUCCESS);
+            roles.getRole().add(role.getRole());
+        } else {
+            roles = roleApi.getRoles();
+            checkSuccess(roles);
+        }
+
+        XmlUtil.serialize(roles, System.out);
+    }
+
+    public static void main(String[] args) throws Exception {
+        try {
+            listRoles(args);
+        } catch (Exception e) {
+            System.err.println("Error listing users: " + e.getMessage());
+            System.exit(-1);
+        }
+    }
+}
+
---
+++ src/org/hyperic/hq/hqapi1/tools/RoleSync.java
@@ -0,0 +1,45 @@
+package org.hyperic.hq.hqapi1.tools;
+
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+import org.hyperic.hq.hqapi1.HQApi;
+import org.hyperic.hq.hqapi1.XmlUtil;
+import org.hyperic.hq.hqapi1.RoleApi;
+import org.hyperic.hq.hqapi1.types.StatusResponse;
+import org.hyperic.hq.hqapi1.types.RolesResponse;
+import org.hyperic.hq.hqapi1.types.Role;
+
+import java.util.List;
+
+public class RoleSync extends ToolsBase {
+
+    private static void syncRoles(String[] args) throws Exception {
+
+        OptionParser p = getOptionParser();
+        OptionSet options = getOptions(p, args);
+
+        HQApi api = getApi(options);
+
+        RoleApi roleApi = api.getRoleApi();
+
+        RolesResponse resp = XmlUtil.deserialize(RolesResponse.class,
+                                                 System.in);
+        List<Role> roles = resp.getRole();
+
+        StatusResponse syncResponse = roleApi.syncRoles(roles);
+        checkSuccess(syncResponse);
+
+        System.out.println("Successfully synced " + roles.size() + " roles.");
+    }
+
+    public static void main(String[] args) throws Exception {
+        try {
+            syncRoles(args);
+        } catch (Exception e) {
+            System.err.println("Error listing users: " + e.getMessage());
+            System.exit(-1);
+        }
+    }
+}
+
+
