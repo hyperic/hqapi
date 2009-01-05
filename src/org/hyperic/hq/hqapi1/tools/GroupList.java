95b8df77192b766c822cb1c78f217647ac65b5bd
Ryan Morgan <rmorgan@hyperic.com>
2009-01-05 17:01:14 +0000

    Add --compatible and --mixed flags for group list.

--- src/org/hyperic/hq/hqapi1/tools/GroupList.java
+++ src/org/hyperic/hq/hqapi1/tools/GroupList.java
@@ -9,16 +9,36 @@ import org.hyperic.hq.hqapi1.types.GroupsResponse;
 
 public class GroupList extends ToolsBase {
 
+    private static String OPT_COMPAT = "compatible";
+    private static String OPT_MIXED = "mixed";
+
     private static void listGroups(String[] args) throws Exception {
 
         OptionParser p = getOptionParser();
+
+        p.accepts(OPT_COMPAT, "List only compatible groups");
+        p.accepts(OPT_MIXED, "List only mixed groups");
 
         OptionSet options = getOptions(p, args);
 
         HQApi api = getApi(options);
         GroupApi groupApi = api.getGroupApi();
 
-        GroupsResponse groups = groupApi.getGroups();
+        if (options.has(OPT_COMPAT) && options.has(OPT_MIXED)) {
+            System.err.println("Only one of " + OPT_COMPAT + " and " + OPT_MIXED +
+                               " is allowed.");
+            System.exit(-1);
+        }
+         
+        GroupsResponse groups;
+
+        if (options.has(OPT_COMPAT)) {
+            groups = groupApi.getCompatibleGroups();
+        } else if (options.has(OPT_MIXED)) {
+            groups = groupApi.getMixedGroups();
+        } else {
+            groups = groupApi.getGroups();
+        }
 
         XmlUtil.serialize(groups, System.out, Boolean.TRUE);
     }
