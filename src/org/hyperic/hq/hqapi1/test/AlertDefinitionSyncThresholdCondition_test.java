cb3c3576465f924ea052fa911d93ca075265b75d
Ryan Morgan <rmorgan@hyperic.com>
2009-01-16 03:55:10 +0000

    Add AlertDef Threshold tests.

---
+++ src/org/hyperic/hq/hqapi1/test/AlertDefinitionSyncThresholdCondition_test.java
@@ -0,0 +1,173 @@
+package org.hyperic.hq.hqapi1.test;
+
+import org.hyperic.hq.hqapi1.AlertDefinitionApi;
+import org.hyperic.hq.hqapi1.AlertDefinitionBuilder;
+import org.hyperic.hq.hqapi1.AlertDefinitionBuilder.AlertConditionType;
+import org.hyperic.hq.hqapi1.AlertDefinitionBuilder.AlertComparator;
+import org.hyperic.hq.hqapi1.HQApi;
+import org.hyperic.hq.hqapi1.MetricApi;
+import org.hyperic.hq.hqapi1.types.AlertCondition;
+import org.hyperic.hq.hqapi1.types.AlertDefinition;
+import org.hyperic.hq.hqapi1.types.AlertDefinitionsResponse;
+import org.hyperic.hq.hqapi1.types.Metric;
+import org.hyperic.hq.hqapi1.types.MetricsResponse;
+import org.hyperic.hq.hqapi1.types.Resource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AlertDefinitionSyncThresholdCondition_test extends AlertDefinitionTestBase {
+
+    public AlertDefinitionSyncThresholdCondition_test(String name) {
+        super(name);
+    }
+
+    public void testValidThresholdConditon() throws Exception {
+
+        HQApi api = getApi();
+        AlertDefinitionApi defApi = api.getAlertDefinitionApi();
+        MetricApi metricApi = api.getMetricApi();
+
+        Resource platform = getLocalPlatformResource(false, false);
+
+        MetricsResponse metricsResponse = metricApi.getMetrics(platform);
+        hqAssertSuccess(metricsResponse);
+        assertTrue("No metrics found for " + platform.getName(),
+                metricsResponse.getMetric().size() > 0);
+        Metric m = metricsResponse.getMetric().get(0);
+
+        AlertDefinition d = generateTestDefinition();
+        d.setResource(platform);
+        final double THRESHOLD = 0;
+        d.getAlertCondition().add(
+                AlertDefinitionBuilder.createThresholdCondition(true, m.getName(),
+                                                                AlertComparator.GREATER_THAN,
+                                                                THRESHOLD));
+
+        List<AlertDefinition> defintions = new ArrayList<AlertDefinition>();
+        defintions.add(d);
+        AlertDefinitionsResponse response = defApi.syncAlertDefinitions(defintions);
+        hqAssertSuccess(response);
+
+        for (AlertDefinition def : response.getAlertDefinition()) {
+            validateDefinition(d);
+            for (AlertCondition c : def.getAlertCondition()) {
+                assertNotNull("Condition was null", c);
+                assertEquals(c.getType(), AlertConditionType.THRESHOLD.getType());
+                assertEquals(c.getThresholdComparator(),
+                             AlertComparator.GREATER_THAN.getComparator());
+                assertEquals(c.getThresholdMetric(), m.getName());
+                assertEquals(c.getThresholdValue(), THRESHOLD);
+            }
+        }
+
+        // cleanup
+        cleanup(response.getAlertDefinition());
+    }
+
+    public void testValidThresholdConditonTypeAlert() throws Exception {
+
+        HQApi api = getApi();
+        AlertDefinitionApi defApi = api.getAlertDefinitionApi();
+        MetricApi metricApi = api.getMetricApi();
+
+        Resource platform = getLocalPlatformResource(false, false);
+
+        MetricsResponse metricsResponse = metricApi.getMetrics(platform);
+        hqAssertSuccess(metricsResponse);
+        assertTrue("No metrics found for " + platform.getName(),
+                metricsResponse.getMetric().size() > 0);
+        Metric m = metricsResponse.getMetric().get(0);
+
+        AlertDefinition d = generateTestDefinition();
+        d.setResource(platform);
+        final double THRESHOLD = 0;
+        d.getAlertCondition().add(
+                AlertDefinitionBuilder.createThresholdCondition(true, m.getName(),
+                                                                AlertComparator.GREATER_THAN,
+                                                                THRESHOLD));
+
+        List<AlertDefinition> defintions = new ArrayList<AlertDefinition>();
+        defintions.add(d);
+        AlertDefinitionsResponse response = defApi.syncAlertDefinitions(defintions);
+        hqAssertSuccess(response);
+
+        for (AlertDefinition def : response.getAlertDefinition()) {
+            validateDefinition(d);
+            for (AlertCondition c : def.getAlertCondition()) {
+                assertNotNull("Condition was null", c);
+                assertEquals(c.getType(), AlertConditionType.THRESHOLD.getType());
+                assertEquals(c.getThresholdComparator(),
+                             AlertComparator.GREATER_THAN.getComparator());
+                assertEquals(c.getThresholdMetric(), m.getName());
+                assertEquals(c.getThresholdValue(), THRESHOLD);
+            }
+        }
+
+        // cleanup
+        cleanup(response.getAlertDefinition());
+    }
+
+    public void testCreateMissingAttributes() throws Exception {
+
+        HQApi api = getApi();
+        AlertDefinitionApi defApi = api.getAlertDefinitionApi();
+        MetricApi metricApi = api.getMetricApi();
+
+        Resource platform = getLocalPlatformResource(false, false);
+
+        MetricsResponse metricsResponse = metricApi.getMetrics(platform);
+        hqAssertSuccess(metricsResponse);
+        assertTrue("No metrics found for " + platform.getName(),
+                metricsResponse.getMetric().size() > 0);
+        Metric m = metricsResponse.getMetric().get(0);
+
+        AlertDefinition d = generateTestDefinition();
+        d.setResourcePrototype(platform.getResourcePrototype());
+        AlertCondition c = new AlertCondition();
+        c.setType(AlertConditionType.THRESHOLD.getType());
+        d.getAlertCondition().add(c);
+
+        List<AlertDefinition> definitions = new ArrayList<AlertDefinition>();
+        definitions.add(d);
+        AlertDefinitionsResponse response = defApi.syncAlertDefinitions(definitions);
+        hqAssertFailureInvalidParameters(response);
+
+        c.setThresholdComparator(AlertComparator.EQUALS.getComparator());
+        response = defApi.syncAlertDefinitions(definitions);
+        hqAssertFailureInvalidParameters(response);
+
+        c.setThresholdMetric(m.getName());
+        response = defApi.syncAlertDefinitions(definitions);
+        hqAssertFailureInvalidParameters(response);
+
+        final double THRESHOLD = 0;
+        c.setThresholdValue(THRESHOLD);
+        response = defApi.syncAlertDefinitions(definitions);
+        hqAssertSuccess(response);
+
+        // cleanup
+        cleanup(response.getAlertDefinition());
+    }
+
+    public void testCreateInvalidMetric() throws Exception {
+
+        HQApi api = getApi();
+        AlertDefinitionApi defApi = api.getAlertDefinitionApi();
+
+        Resource platform = getLocalPlatformResource(false, false);
+
+        AlertDefinition d = generateTestDefinition();
+        d.setResource(platform);
+        final double THRESHOLD = 0;
+        d.getAlertCondition().add(
+                AlertDefinitionBuilder.createThresholdCondition(true, "Invalid Metric Name",
+                                                                AlertComparator.GREATER_THAN,
+                                                                THRESHOLD));
+
+        List<AlertDefinition> defintions = new ArrayList<AlertDefinition>();
+        defintions.add(d);
+        AlertDefinitionsResponse response = defApi.syncAlertDefinitions(defintions);
+        hqAssertFailureObjectNotFound(response);
+    }
+}
