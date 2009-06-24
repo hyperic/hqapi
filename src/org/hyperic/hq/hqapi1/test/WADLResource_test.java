c1697f420be8e2afb76bc07c36005c4d2d81eb19
Ryan Morgan <rmorgan@hyperic.com>
2009-06-24 14:01:11 -0700

    Add WADL resource definitions for ResourceApi.

---
+++ src/org/hyperic/hq/hqapi1/test/WADLResource_test.java
@@ -0,0 +1,11 @@
+package org.hyperic.hq.hqapi1.test;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: rmorgan
+ * Date: Jun 24, 2009
+ * Time: 1:39:51 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class WADLResource_test {
+}
