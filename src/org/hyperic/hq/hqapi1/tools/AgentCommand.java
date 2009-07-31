dc650e423529d99a83a0164c82dfb114fbcdbb12
Ryan Morgan <rmorgan@hyperic.com>
2009-07-31 15:28:08 -0700

    Check command success for agent list before listing XML.

--- src/org/hyperic/hq/hqapi1/tools/AgentCommand.java
+++ src/org/hyperic/hq/hqapi1/tools/AgentCommand.java
@@ -72,6 +72,7 @@ public class AgentCommand extends Command {
         AgentApi agentApi = api.getAgentApi();
 
         AgentsResponse agents = agentApi.getAgents();
+        checkSuccess(agents);
 
         XmlUtil.serialize(agents, System.out, Boolean.TRUE);
     }
