393e7293aaee118eae9961f077d4b5d5aed9a44f
Ryan Morgan <rmorgan@hyperic.com>
2009-10-01 15:09:47 -0700

    Add tests for finding alerts within millisecond ranges.

--- src/org/hyperic/hq/hqapi1/test/AlertFindByResource_test.java
+++ src/org/hyperic/hq/hqapi1/test/AlertFindByResource_test.java
@@ -12,10 +12,9 @@ public class AlertFindByResource_test extends AlertTestBase {
     }
 
     public void testFindValid() throws Exception {
+        AlertApi api = getAlertApi();
         Resource r = getLocalPlatformResource(false, false);
         Alert a = generateAlerts(r);
-
-        AlertApi api = getAlertApi();
 
         AlertsResponse response = api.findAlerts(r, 0, System.currentTimeMillis(),
                                                  10, 1, false, false);
@@ -31,25 +30,66 @@ public class AlertFindByResource_test extends AlertTestBase {
         deleteAlertDefinitionByAlert(a);
     }
 
-    public void testFindInvalidSeverity() throws Exception {
-        Resource r = getLocalPlatformResource(false, false);
+    public void testFindValidSmallRange() throws Exception {
         AlertApi api = getAlertApi();
+        Resource r = getLocalPlatformResource(false, false);
+        Alert a = generateAlerts(r);
+
+        // Issue find 1 ms < a.ctime and 1 ms > a.ctime
+        long start = a.getCtime() - 1;
+        long end = a.getCtime() + 1;
+
+        AlertsResponse response = api.findAlerts(r, start, end,
+                                                 10, 1, false, false);
+        hqAssertSuccess(response);
+        assertTrue(response.getAlert().size() <= 10);
+        assertTrue(response.getAlert().size() > 0);
+
+        for (Alert alerts : response.getAlert()) {
+            validateAlert(alerts);
+        }
+
+        // Cleanup
+        deleteAlertDefinitionByAlert(a);
+    }
+
+    public void testFindSmallRangeNoAlerts() throws Exception {
+        AlertApi api = getAlertApi();
+        Resource r = getLocalPlatformResource(false, false);
+        Alert a = generateAlerts(r);
+
+        // Issue find 1 ms > a.ctime and 2 ms > a.ctime
+        long start = a.getCtime() + 1;
+        long end = a.getCtime() + 2;
+
+        AlertsResponse response = api.findAlerts(r, start, end,
+                                                 10, 1, false, false);
+        hqAssertSuccess(response);
+        assertTrue(response.getAlert().size() == 0);
+
+        // Cleanup
+        deleteAlertDefinitionByAlert(a);
+    }
+
+    public void testFindInvalidSeverity() throws Exception {
+        AlertApi api = getAlertApi();
+        Resource r = getLocalPlatformResource(false, false);
         AlertsResponse response = api.findAlerts(r, 0, System.currentTimeMillis(),
                                                  10, 4, false, false);
         hqAssertFailureInvalidParameters(response);
     }
 
     public void testFindInvalidCount() throws Exception {
-        Resource r = getLocalPlatformResource(false, false);
         AlertApi api = getAlertApi();
+        Resource r = getLocalPlatformResource(false, false);
         AlertsResponse response = api.findAlerts(r, 0, System.currentTimeMillis(),
                                                  -5, 2, false, false);
         hqAssertFailureInvalidParameters(response);
     }
 
     public void testFindInvalidRange() throws Exception {
-        Resource r = getLocalPlatformResource(false, false);
         AlertApi api = getAlertApi();
+        Resource r = getLocalPlatformResource(false, false);
 
         AlertsResponse response = api.findAlerts(r, System.currentTimeMillis(), 0,
                                                  10, 2, false, false);
@@ -57,10 +97,11 @@ public class AlertFindByResource_test extends AlertTestBase {
     }
 
     public void testFindInvalidResource() throws Exception {
+        AlertApi api = getAlertApi();
+
         Resource r = new Resource();
         r.setId(Integer.MAX_VALUE);
         r.setName("Invalid resource");
-        AlertApi api = getAlertApi();
         AlertsResponse response = api.findAlerts(r, 0, System.currentTimeMillis(),
                                                  10, 1, false, false);
         hqAssertFailureObjectNotFound(response);
--- src/org/hyperic/hq/hqapi1/test/AlertFind_test.java
+++ src/org/hyperic/hq/hqapi1/test/AlertFind_test.java
@@ -30,6 +30,41 @@ public class AlertFind_test extends AlertTestBase {
         deleteAlertDefinitionByAlert(a);
     }
 
+    public void testFindValidSmallRange() throws Exception {
+        AlertApi api = getAlertApi();
+        Resource platform = getLocalPlatformResource(false, false);
+        Alert a = generateAlerts(platform);
+
+        long start = a.getCtime() - 1;
+        long end = a.getCtime() + 1;
+
+        AlertsResponse response = api.findAlerts(start, end,
+                                                 10, 1, false, false);
+        hqAssertSuccess(response);
+        assertTrue(response.getAlert().size() <= 10);
+        assertTrue(response.getAlert().size() > 0);
+
+        for (Alert alerts : response.getAlert()) {
+            validateAlert(alerts);
+        }
+
+        // Cleanup
+        deleteAlertDefinitionByAlert(a);
+    }
+
+    public void testFindValidSmallRangeNoAlerts() throws Exception {
+        AlertApi api = getAlertApi();
+
+        // Issue a find slightly into the future, should return 0 alerts
+        long start = System.currentTimeMillis() + 100;
+        long end = start + 1;
+
+        AlertsResponse response = api.findAlerts(start, end,
+                                                 10, 1, false, false);
+        hqAssertSuccess(response);
+        assertTrue(response.getAlert().size() == 0);
+    }
+
     public void testFindInvalidSeverity() throws Exception {
         AlertApi api = getAlertApi();
         AlertsResponse response = api.findAlerts(0, System.currentTimeMillis(),
