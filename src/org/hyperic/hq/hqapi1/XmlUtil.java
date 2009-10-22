5ef71a8acb79364a8016baf87952c3eecd7273ed
Jennifer Hickey <jhickey@calculon.local>
2009-10-22 13:54:16 -0500

    Modified parser to select JAXBContext based on package (for extension)

--- src/org/hyperic/hq/hqapi1/XmlUtil.java
+++ src/org/hyperic/hq/hqapi1/XmlUtil.java
@@ -40,21 +40,12 @@ import java.io.OutputStream;
  */
 public class XmlUtil {
 
-    private static JAXBContext CTX;
-
-    static {
-        try {
-            CTX = JAXBContext.newInstance("org.hyperic.hq.hqapi1.types");
-        } catch (JAXBException e) {
-            // Not going to happen
-            System.out.println("Error initializing context: " + e.getMessage());
-        }
-    }
-
     public static <T> T deserialize(Class<T> res, InputStream is)
         throws JAXBException
     {
-        Unmarshaller u = CTX.createUnmarshaller();
+        String pkg = res.getPackage().getName();
+        JAXBContext jc = JAXBContext.newInstance(pkg);
+        Unmarshaller u = jc.createUnmarshaller();
         u.setEventHandler(new DefaultValidationEventHandler());
         return res.cast(u.unmarshal(is));
     }
@@ -62,9 +53,11 @@ public class XmlUtil {
     public static void serialize(Object o, OutputStream os, Boolean format)
         throws JAXBException
     {
-        Marshaller m = CTX.createMarshaller();
+        String pkg = o.getClass().getPackage().getName();
+        JAXBContext jc = JAXBContext.newInstance(pkg);
+        Marshaller m = jc.createMarshaller();
         m.setEventHandler(new DefaultValidationEventHandler());
-        m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, format);
+        m.setProperty("jaxb.formatted.output", format);
         m.marshal(o, os);
     }
 }
