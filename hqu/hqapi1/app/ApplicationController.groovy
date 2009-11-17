008d7527ac8c2194e4286d2595accfb010ba9bcf
Jennifer Hickey <jennifer.hickey@springsource.com>
2009-11-16 17:07:05 -0800

    Converted ApplicationManager EJB

--- hqu/hqapi1/app/ApplicationController.groovy
+++ hqu/hqapi1/app/ApplicationController.groovy
@@ -1,5 +1,5 @@
 import org.hyperic.hq.hqapi1.ErrorCode
-import org.hyperic.hq.appdef.server.session.ApplicationManagerEJBImpl as AppMan
+import org.hyperic.hq.appdef.server.session.ApplicationManagerImpl as AppMan
 import org.hyperic.hq.bizapp.server.session.AppdefBossImpl as ABoss
 import org.hyperic.hq.authz.server.session.ResourceManagerImpl as ResMan
 import org.hyperic.util.pager.PageControl
