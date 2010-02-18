0f54ac5395c005e77f74ade750b0dced6cffe588
Ryan Morgan <rmorgan@hyperic.com>
2010-02-18 10:12:37 -0800

    [HHQ-3740] Allow hqapi.sh to be executed from a symlinked directory.

--- bin/hqapi.sh
+++ bin/hqapi.sh
@@ -3,6 +3,6 @@
 cd `dirname $0`/..
 RUNDIR=`pwd`
 
-CLASSPATH=$CLASSPATH$(find $RUNDIR -name "*.jar" -exec printf :{} ';')
+CLASSPATH=$CLASSPATH$(find -L $RUNDIR -name "*.jar" -exec printf :{} ';')
 
 java -cp $CLASSPATH org.hyperic.hq.hqapi1.tools.Shell "$@"
