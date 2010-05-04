2b09dfee7f9fbffa84b8dad91f67864c94a9df3d
Ryan Morgan <rmorgan@hyperic.com>
2010-05-04 10:27:53 -0700

    Fix help documentation for --assignScriptAction

--- src/org/hyperic/hq/hqapi1/tools/AlertDefinitionCommand.java
+++ src/org/hyperic/hq/hqapi1/tools/AlertDefinitionCommand.java
@@ -281,7 +281,7 @@ public class AlertDefinitionCommand extends Command {
         p.accepts(OPT_ASSIGN_ESC, "If specified, assign the given Escalation " +
                                    "to all alert definitions in this sync").
                 withRequiredArg().ofType(String.class);
-        p.accepts(OPT_ASSIGN_SCRIPTACTION, "If specified, assign the given Escalation " +
+        p.accepts(OPT_ASSIGN_SCRIPTACTION, "If specified, assign the given script action " +
                                            "to all alert definitions in this sync").
                 withRequiredArg().ofType(String.class);
         p.accepts(OPT_ASSIGN_CONTROLACTION, "If specified, assign the given Escalation " +
